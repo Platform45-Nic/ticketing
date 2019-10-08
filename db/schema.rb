3da62e9bcdbf045608a074b80bfb0b3a28e7940b
Platform45-Nic <nicolo@platform45.com>
2019-10-08 17:43:11 +0200

    Generated Events model and controller.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191008135331) do
+ActiveRecord::Schema.define(version: 20191008154115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "catagories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.integer "number_of_tickets"
+    t.date "date"
+    t.string "catagory"
+    t.text "description"
+    t.string "creator"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
