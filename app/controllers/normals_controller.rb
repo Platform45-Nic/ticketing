54f9b597f24ada058072213a884315b829e976b4
Platform45-Nic <nicolo@platform45.com>
2019-10-15 16:52:16 +0200

    Added accounts_controller. Added accounts routing to scoped user routes. Added index and show views. Added account creation in normals_controller to create account and default account_transaction.

--- app/controllers/normals_controller.rb
+++ app/controllers/normals_controller.rb
@@ -5,6 +5,21 @@ class NormalsController < ApplicationController
 
   def show
     @user = User.find(current_user.id)
+    if @account = Account.find(user_id: @user.id).blank?
+      account_init
+    else
+      @account = Account.find(user_id: @user.id)
+      @account_transactions = AccountTransaction.where(account_id: @account.id)
+    end
   end
+
+  private
   
+  def account_init
+    if current_user.present? && current_user.type == "Normal"
+      @account_new = Account.new(user_id: User.find(current_user.id).id, amount: 100).save
+      AccountTransaction.new(amount: 100, account_id: @account_new.id.to_i, note: "initialization").save
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   validates_presence_of :email
   validates_presence_of :first_name
   validates_presence_of :last_name
-  after_initialize :init
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -16,13 +15,6 @@ class User < ApplicationRecord
 
   scope :admin, -> { where(type: 'Admin')}
   scope :normal, -> { where(type: 'Normal')}
-
-  def init
-    if current_user.type == "Normal"
-      @account = Account.new(user_id: User.find(current_user.id).id, amount: 100).save
-      AccountTransaction.new(amount: 100, account_id: @account.id.to_i, note: "initialization").save
-    end
-  end
 
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
