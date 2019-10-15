e300c6c63dd81f687442fd9d66c6dfc10edad74a
Platform45-Nic <nicolo@platform45.com>
2019-10-15 12:37:42 +0200

    Created generated model for account. Added reference to user and default for amount.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191010120311) do
+ActiveRecord::Schema.define(version: 20191015103426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "amount", default: 100, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "catagories", force: :cascade do |t|
     t.string "name"
@@ -63,6 +71,7 @@ ActiveRecord::Schema.define(version: 20191010120311) do
     t.index ["type"], name: "index_users_on_type"
   end
 
+  add_foreign_key "accounts", "users"
   add_foreign_key "events", "catagories"
   add_foreign_key "tickets", "events"
   add_foreign_key "tickets", "users", column: "purchaser_id"
---
+++ spec/factories/accounts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :account do
+    user { nil }
+    amount { 1 }
+  end
+end
